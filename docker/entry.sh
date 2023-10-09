c419500f7b7b4d9a389f5faf200572924d6cdf43
Arpit Tripathi (a04t) <arpit04tripathi@gmail.com>
2023-10-09 09:53:23 +0100

    update entry sh

--- docker/entry.sh
+++ docker/entry.sh
@@ -1,5 +1,13 @@
 echo "Running entry.sh"
 # echo "<=== info minima ===>"
 # bundle info minima
+
+# echo "<=== gem install bundler ===>"
+# gem install bundler
+# echo "<=== bundle install ===>"
+# bundle install
+# echo "<=== bundle exec jekyll serve --incremental ===>"
+# bundle exec jekyll serve --incremental
+
 echo "<=== serve incremental ===>"
 jekyll serve --incremental
